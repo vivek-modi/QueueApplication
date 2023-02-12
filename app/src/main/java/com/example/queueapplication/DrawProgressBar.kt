ab6824dbec34a61d5e16dd89780cade0a5868553
Vivek Modi <vivekmodi456@gmail.com>
2023-02-12 22:27:38 +0000

    added new gradient color

--- app/src/main/java/com/example/queueapplication/DrawProgressBar.kt
+++ app/src/main/java/com/example/queueapplication/DrawProgressBar.kt
@@ -1,5 +1,6 @@
 package com.example.queueapplication
 
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -8,16 +9,21 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun DrawProgressBar() {
+    val activity = LocalContext.current as AppCompatActivity
     val rangeComposition = RangeComposition()
     val itemLst = rangeComposition.bpExplained
     val boxSize = 30.dp
+    val brush = Brush.horizontalGradient(listOf(Color.Red, Color.Blue))
+    val progressBarPointer = rangeComposition.findReadingWithPointer(142, 90).second
     Box(
         modifier = Modifier
             .background(Color.White)
@@ -34,6 +40,15 @@ fun DrawProgressBar() {
                 start = Offset(x = 0f, y = canvasHeight / 2),
                 end = Offset(x = canvasWidth, y = canvasHeight / 2),
                 color = Color.Gray,
+                strokeWidth = strokeWidthPx,
+                cap = StrokeCap.Round,
+            )
+            val progressBarPointerInPixel = (progressBarPointer / 100f) * canvasWidth
+            activity.logE("progressBarPointerInPixel $progressBarPointerInPixel")
+            drawLine(
+                brush = brush,
+                start = Offset(x = 0f, y = canvasHeight / 2),
+                end = Offset(x = progressBarPointerInPixel, y = canvasHeight / 2),
                 strokeWidth = strokeWidthPx,
                 cap = StrokeCap.Round,
             )
