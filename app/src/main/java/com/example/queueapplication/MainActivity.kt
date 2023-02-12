c377725fea82565ef0dc1d2b5fd5a470e3993287
Kritika Dhiman <a00268745@student.ait.ie>
2023-02-12 14:18:02 +0000

    added compose version

--- app/src/main/java/com/example/queueapplication/MainActivity.kt
+++ app/src/main/java/com/example/queueapplication/MainActivity.kt
@@ -3,19 +3,20 @@ package com.example.queueapplication
 import android.Manifest
 import android.os.Build
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.lifecycleScope
-import com.example.queueapplication.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
     private val viewModel: QueueViewModel by viewModels()
     private val RECORD_REQUEST_CODE = 101
 
@@ -32,10 +33,14 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 //        makeRequest()
         startWorking()
+
+        setContent {
+            Theme {
+                Text(text = "Hi", color = Color.White)
+            }
+        }
     }
 
     private fun startWorking() {
---
+++ app/src/main/java/com/example/queueapplication/Theme.kt
@@ -0,0 +1,9 @@
+package com.example.queueapplication
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Theme(content: @Composable () -> Unit) {
+    MaterialTheme(content = content)
+}
